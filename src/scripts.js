30c4e9de4fb675e801fdd70c59659ed9eff73fdb
Thibaut <thibautdebroucke@gmail.com>
2023-01-04 10:14:36 +0100

    modifs

--- src/scripts.js
+++ src/scripts.js
@@ -3,26 +3,25 @@ req.addEventListener("load", evt => {
     let data = JSON.parse(req.responseText);
     makeTableau(data)
 });
-req.open("GET", "data.json");
+req.open("GET", "https://www.cril.univ-artois.fr/~lecoutre/teaching/jssae/code5/results.json");
 req.send()
 
 function makeTableau(data) {
     let liste = document.querySelector('ol')
-    liste.innerHTML = "<h1>Puzzles</h1>"
-    let tab = data[0].data
+    liste.innerHTML = "<h1>Puzzles - SAE303</h1>"
+    let tab = data[2].data
     let puzzles = []
     for (element of tab) {
-        if (puzzles.includes(element.family) == false)
-            puzzles.push(element.family)
+        if (puzzles.includes("<p>" + element.family + "</p>") == false)
+            puzzles.push("<p>" + element.family + "</p>")
     }
-
 
     for (let i = 0; i < puzzles.length; i++) {
         let family = document.createElement('ul')
         let souspuzzles = []
-        family.innerHTML = puzzles[i]
+        family.innerHTML = "<a href='#' onmouseover=document.getElementById('test').style.display=block; onmouseout=document.getElementById('test').style.display=none; >" + puzzles[i] + "</a><div id=test style=display:none; width:500px; height:500px; background-color:blue;>"
         for (element of tab) {
-            if (element.family == puzzles[i]) {
+            if ("<p>" + element.family + "</p>" == puzzles[i]) {
                 if (souspuzzles.includes(element.fullname) == false) {
                     souspuzzles.push(element.fullname)
                     let sousfamily = document.createElement('li')
@@ -32,7 +31,6 @@ function makeTableau(data) {
             }
         }
         liste.appendChild(family)
-
     }
 
 
